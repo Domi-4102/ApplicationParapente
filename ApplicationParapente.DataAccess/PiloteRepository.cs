fbbf1af3e06502095e8502b5031e509e055c251b
steve ramackers <steve_ramackers@hotmail.com>
2020-02-08 17:41:44 +0100

    add PiloteRepository

--- ApplicationParapente.DataAccess/PiloteRepository.cs
+++ ApplicationParapente.DataAccess/PiloteRepository.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ApplicationParapente.DataAccess.Interfaces;
+using Parapente.Data.Entities;
 
 namespace ApplicationParapente.DataAccess
 {
-    class PiloteRepository
+    public class PiloteRepository : IPiloteRepository
     {
+        void IRepository<Pilote, int>.Delete(Pilote entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Pilote> IRepository<Pilote, int>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Pilote IRepository<Pilote, int>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Pilote, int>.Insert(Pilote entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<Pilote, int>.Update(Pilote entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
