36a47581e8d59ebda5562d9af5fdf079782031b8
steve ramackers <steve_ramackers@hotmail.com>
2020-02-09 00:40:38 +0100

    update implement repositoryPattern

--- ApplicationParapente.DataAccess/PiloteRepository.cs
+++ ApplicationParapente.DataAccess/PiloteRepository.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using ApplicationParapente.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Parapente.Data;
 using Parapente.Data.Entities;
 
 namespace ApplicationParapente.DataAccess
 {
     public class PiloteRepository : IPiloteRepository
     {
+        private readonly ParapenteContext _dataContext;
         void IRepository<Pilote, int>.Delete(Pilote entity)
         {
-            throw new NotImplementedException();
+            _dataContext.Set<Pilote>().Remove(entity);
         }
 
         IEnumerable<Pilote> IRepository<Pilote, int>.GetAll()
         {
-            throw new NotImplementedException();
+            return _dataContext.Set<Pilote>().ToList();
         }
 
         Pilote IRepository<Pilote, int>.GetById(int id)
         {
-            throw new NotImplementedException();
+            return _dataContext.Set<Pilote>().FirstOrDefault(e => e.IdPilote == id);
         }
 
         void IRepository<Pilote, int>.Insert(Pilote entity)
         {
-            throw new NotImplementedException();
+            _dataContext.Set<Pilote>().Add(entity);
         }
 
         void IRepository<Pilote, int>.Update(Pilote entity)
         {
-            throw new NotImplementedException();
+            var editedEntity = _dataContext.Set<Pilote>().FirstOrDefault(e => e.IdPilote == entity.IdPilote);
         }
     }
 }
---
+++ ApplicationParapente/Models/Pilote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationParapente.Models
+{
+    public class Pilote
+    {
+        #region Properties
+        public int IdPilote { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        #endregion
+    }
+}
