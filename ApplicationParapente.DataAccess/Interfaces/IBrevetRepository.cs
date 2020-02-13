5754ce39421fbe9226ee5e614141971d8678e4d3
Domi4102 <steve_ramackers@hotmail.com>
2020-02-13 15:47:37 +0100

    Add brevet

---
+++ ApplicationParapente.DataAccess/Interfaces/IBrevetRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Parapente.Data.Entities;
+
+namespace ApplicationParapente.DataAccess.Interfaces
+{
+    public interface IBrevetRepository : IRepository<Brevet, int>
+    {
+    }
+}
